1bc85df26face4830240be3d94400eed55be2ad3
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2024-01-25 17:43:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let formEle = document.getElementById("form");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", func);
+
+function func() {
+  let age = formEle.childNodes[3].value;
+  let name = formEle.childNodes[7].value;
+  console.log(formEle.childNodes);
+  let promise = new Promise((resolve, reject) => {
+    if (age > 18) return resolve(`Welcome, ${name}. You can vote.`);
+    else return reject(`Oh sorry ${name}. You aren't old enough`);
+  });
+
+  promise
+    .then((data) => {
+      alert(data);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
